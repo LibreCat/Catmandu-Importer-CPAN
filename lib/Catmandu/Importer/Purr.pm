c59eb0133243c20ec87015d24364f7034a2d2391
Jakob Voss <voss@gbv.de>
2014-10-17 21:38:13 +0200

    support query by author, remove default prefix

--- lib/Catmandu/Importer/Purr.pm
+++ lib/Catmandu/Importer/Purr.pm
@@ -8,7 +8,8 @@ with 'Catmandu::Importer';
 
 our $VERSION = '0.03';
 
-has prefix => (is => 'ro' , default => sub { "Catmandu" });
+has prefix => (is => 'ro');
+has author => (is => 'ro');
 
 # http://api.metacpan.org/v0/release/_mapping
 our $RELEASE_FIELDS = [qw(
@@ -28,37 +29,48 @@ has fields => (
 );
 
 has mcpan => (is => 'ro', builder => sub { MetaCPAN::API::Tiny->new });
+has filter => (is => 'ro', lazy => 1, builder => 1);
+
+sub _build_filter {
+    my $self = $_[0];
+
+    my @filter = { term => { status => 'latest' } };
+
+    if ($self->prefix) {
+        push @filter, { prefix => { archive => $self->prefix } };
+    }
+
+    if ($self->author) {
+        push @filter, { term => { author => $self->author } };
+    }
+       
+    if (@filter > 1) {
+        return { and => \@filter };
+    } else {
+        return $filter[0];
+    }
+}
 
 sub generator {
-	my $self = $_[0];
-	my $prefix = $self->prefix;
-	my $result = $self->mcpan->post(
-        'release',
-        {   
-            query  => {
-        	  bool => {
-            	should => [{
-                	term => {
-                    	"release.status" => "latest"
-                	}
-            	}]
-              }
-        	},
+    my $self = $_[0];
+
+    my $result = $self->mcpan->post( 
+        release => {
             fields => $self->fields,
+            filter => $self->filter,
             size   => 1024,
-            filter => { prefix => { archive => $prefix } },
-        },
- 	);
+        }
+    );
+ 
+    my @hits = @{$result->{hits}->{hits}};
 
-	my @hits = @{$result->{hits}->{hits}};
-
-	return sub {
-		my $hit = shift @hits;
-		return undef unless $hit;
-		return {
+    return sub {
+        my $hit = shift @hits;
+        return undef unless $hit;
+        return {
             map { $_ => $hit->{fields}->{$_} } @{$self->fields}
-		}
-	};
+        }
+    };
 }
 
 1;
@@ -70,28 +82,31 @@ Catmandu::Importer::Purr - Perl Utility for Recent Releases
 
 =head1 SYNOPSIS
 
- use Catmandu::Importer::Purr;
-
- my $importer = Catmandu::Importer::Purr->new(prefix => 'Catmandu');
+  use Catmandu::Importer::Purr;
+  my $importer = Catmandu::Importer::Purr->new( prefix => 'Catmandu' );
  
- $importer->each(sub {
-	my $module = shift;
-	print $module->{id} , "\n";
-	print $module->{date} , "\n";
-	print $module->{distribution} , "\n";
-	print $module->{version} , "\n";
-	print $module->{abstract} , "\n";
- });
+  $importer->each(sub {
+     my $module = shift;
+     print $module->{distribution} , "\n";
+     print $module->{version} , "\n";
+     print $module->{date} , "\n";
+  });
  
- # or
+Or with the L<catmandu> command line client:
 
- $ catmandu convert Purr
+  $ catmandu convert Purr --author NICS --fields distribution,date to CSV
 
 =head1 CONFIGURATION
 
 =over
 
 =item prefix
+
+Prefix that releases must start with, e.g. C<Catmandu>.
+
+=item author
+
+Selected author
 
 =item fields
 
