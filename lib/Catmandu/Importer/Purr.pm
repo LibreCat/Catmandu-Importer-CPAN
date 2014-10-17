56de97dcdacecd0d75430cb74475d71fd6c0f39e
Jakob Voss <voss@gbv.de>
2014-10-17 20:55:53 +0200

    provide more fields on request (issue #2)

--- lib/Catmandu/Importer/Purr.pm
+++ lib/Catmandu/Importer/Purr.pm
@@ -6,15 +6,28 @@ use Moo;
 
 with 'Catmandu::Importer';
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 has prefix => (is => 'ro' , default => sub { "Catmandu" });
-has mcpan  => (is => 'ro' , lazy => 1, builder => '_build_mcpan');
 
-sub _build_mcpan {
-    my $self = $_[0];
-    MetaCPAN::API::Tiny->new;
-}
+# http://api.metacpan.org/v0/release/_mapping
+our $RELEASE_FIELDS = [qw(
+    abstract archive author authorized date dependency distribution
+    download_url first id license maturity name provides resources stat status
+    tests version version_numified
+)];
+
+has fields => (
+    is => 'ro', 
+    default => sub { [ qw(id date distribution version abstract) ] },
+    coerce => sub {
+        ref $_[0] ? $_[0] 
+                  : ($_[0] eq 'all' ? $RELEASE_FIELDS 
+                                    : [ split /,/, $_[0] ]);
+    }
+);
+
+has mcpan => (is => 'ro', builder => sub { MetaCPAN::API::Tiny->new });
 
 sub generator {
 	my $self = $_[0];
@@ -31,7 +44,7 @@ sub generator {
             	}]
               }
         	},
-            fields => [ qw(id date distribution version abstract) ],
+            fields => $self->fields,
             size   => 1024,
             filter => { prefix => { archive => $prefix } },
         },
@@ -43,14 +56,13 @@ sub generator {
 		my $hit = shift @hits;
 		return undef unless $hit;
 		return {
-			id           => $hit->{fields}->{id} ,
- 		    date         => $hit->{fields}->{date} ,
- 			distribution => $hit->{fields}->{distribution} ,
- 			version      => $hit->{fields}->{version} ,
- 			abstract     => $hit->{fields}->{abstract} ,
+            map { $_ => $hit->{fields}->{$_} } @{$self->fields}
 		}
 	};
 }
+
+1;
+__END__
 
 =head1 NAME 
 
@@ -75,14 +87,28 @@ Catmandu::Importer::Purr - Perl Utility for Recent Releases
 
  $ catmandu convert Purr
 
-=head1 AUTHORS
+=head1 CONFIGURATION
 
- Patrick Hochstenbach, C<< <patrick.hochstenbach at ugent.be> >>
+=over
+
+=item prefix
+
+=item fields
+
+Array reference or comma separated list of fields to get.  The special value
+C<all> will return all fields.  Set to C<id,date,distribution,version,abstract>
+by default.
+
+=back
+
+=head1 CONTRIBUTORS
+
+Patrick Hochstenbach, C<< <patrick.hochstenbach at ugent.be> >>
+
+Jakob Voß C<< <jakob.voss at gbv.de> >>
 
 =head1 SEE ALSO
 
 L<Catmandu>, L<Catmandu::Importer>
 
 =cut
-
-1;
